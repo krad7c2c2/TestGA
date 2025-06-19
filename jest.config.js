d791dd327a7166cac153c5fdd4982745a40df2d2
Carlos Alberto Orta Cruz <caorta@telstock.net>
2025-06-18 18:39:50 -0600

    correccion test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|@react-navigation)/)'
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/app/$1'
+  }
 };
